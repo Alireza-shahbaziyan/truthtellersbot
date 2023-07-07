520c6edbe6e88931d4f60ef01cfa12108c63ea0e
root <root@spooky-country.aeza.network>
2023-07-07 22:02:41 +0300

    make user model

---
+++ db/models.py
@@ -0,0 +1,15 @@
+from pony import orm
+
+from datetime import datetime
+
+db = orm.Database()
+db.bind(provider='sqlite', filename='../db.sqlite3', create_db=True)
+
+
+class User(db.Entity):
+    id        = orm.PrimaryKey(str) # user_id, ex: 956473054
+    haghs     = orm.Required(int)
+    joined_at = orm.Required(datetime)
+
+
+db.generate_mapping(create_tables=True)
