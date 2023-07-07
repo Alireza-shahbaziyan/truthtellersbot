9f3ed92372e14c8a691d1d9949b177da1e68357f
root <root@spooky-country.aeza.network>
2023-07-07 22:08:32 +0300

    initial user on entry point

--- plugins/entry_point.py
+++ plugins/entry_point.py
@@ -1,5 +1,15 @@
 from pyrogram import Client, filters
 
+from db import do
+
+
+
+@Client.on_message(filters.all, group=-10)
+def gate(_, message):
+    if do.get_user(message.from_user.id) is None:
+        do.add_user(message.from_user.id)
+
+
 @Client.on_message(filters.command('start') & filters.private)
-def start(client, message):
+def start(_, message):
     message.reply_text("Hello")
